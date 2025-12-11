83e3ef837b7ba4f4f15975b6162e40bc5570cb0d
riyyan <riyyanhassan789@gmail.com>
2025-12-11 11:09:57 +0500

    Learned how to link html pages to views to fireoff when a specific url is requested by the browser

--- django_project/project/project/views.py
+++ django_project/project/project/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 
+from django.shortcuts import render
+
 def about(request):
-    '''function reponsing to about url request'''
-    return HttpResponse('about page')
+    '''function reponding to about url request'''
+    # return HttpResponse('about page')
+    return render(request, 'about.html')
 
 def home(request):
-    '''function responsing to home url request'''
-    return HttpResponse('homepage')
+    '''function responding to home url request'''
+    # return HttpResponse('homepage')
+    return render(request, 'homepage.html')
