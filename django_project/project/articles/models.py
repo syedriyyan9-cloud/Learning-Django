accd12cdb924684c0d4e2d42e3ef4a0b3e7a16ac
riyyan <riyyanhassan789@gmail.com>
2025-12-13 11:06:38 +0500

    Learned about models and how they are created and used to define tables inside database.

--- django_project/project/articles/models.py
+++ django_project/project/articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Atricles(models.Modle):
+    '''a class to represent the table for articles inside the database'''
+    title =  models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
