61d7a7d7c89b96add3f08382f545821382f9cd64
riyyan <riyyanhassan789@gmail.com>
2025-12-17 19:07:33 +0500

    Learned how to use template tags using django.

--- django_project/project/articles/views.py
+++ django_project/project/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Atricles
 # Create your views here.
 def home(request):
-    '''renders homepage.html'''
-    return render(request,'articles/homepage.html')
+    '''content is rendered dynamically onto the homepage.html of the app'''
+    articles = Atricles.objects.all()
+    return render(request,'articles/homepage.html', {'articles': articles})
