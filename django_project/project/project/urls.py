901a663a43a129f52873f0a1598c2941b65924e3
riyyan <riyyanhassan789@gmail.com>
2025-12-19 11:43:51 +0500

    Learned how to display static files and images.

--- django_project/project/project/urls.py
+++ django_project/project/project/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +25,5 @@ urlpatterns = [
     path('about/', views.about),
     path('', views.home)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
