c54fd4e37177ad4a0aba1270a44292bd0e69d260
riyyan <riyyanhassan789@gmail.com>
2026-01-06 18:46:58 +0500

    Learned how to log users out of an app

--- django_project/project/accounts/views.py
+++ django_project/project/accounts/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 
 def signup(request):
-    '''signup form'''
+    """signup form"""
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -17,7 +17,7 @@ def signup(request):
     return render(request, 'accounts/signup.html', {"form":form})
 
 def login_view(request):
-    '''login form'''
+    """login form"""
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
@@ -28,3 +28,9 @@ def login_view(request):
     else:
         form = AuthenticationForm
     return render(request, 'accounts/login.html', {'form':form})
+
+def logout_view(request):
+    """log the user out"""
+    if request.method == 'POST':
+        logout(request)
+        return redirect('articles:list')
